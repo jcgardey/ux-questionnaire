9ed9b5ad1ab0cedc5e499642fb242be707598320
Juan Cruz Gardey <juancruzgardey@hotmail.com>
2025-06-03 19:01:50 +0000

    improve success page

--- src/pages/SuccessPage.tsx
+++ src/pages/SuccessPage.tsx
@@ -2,8 +2,21 @@ import { Title } from '@/components/Title';
 
 export const SuccessPage = () => {
   return (
-    <div>
-      <Title variant="h1" text="Gracias por participar!" />
+    <div className="flex flex-col gap-6">
+      <Title variant="h1" text="Cierre y agradecimiento" />
+      <p>
+        Muchas gracias por participar en este ejercicio! Tu contribución es
+        valiosa y nos ayudará a comprender mejor cómo se toman las decisiones de
+        priorización en entornos ágiles. Apreciamos el tiempo que dedicaste a
+        completar esta tarea.
+      </p>
+
+      <p>
+        Si tenés comentarios o preguntas sobre el ejercicio, no dudes en
+        contactarnos.
+      </p>
+
+      <p>¡Gracias nuevamente y que tengas un excelente día!</p>
     </div>
   );
 };
